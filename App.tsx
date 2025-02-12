41fe17a4536b1e9d07d08da0295a1f291620acf7
dbryn24 <davebryangamer@gmail.com>
2025-02-12 14:05:07 +0800

    basic react native

--- App.tsx
+++ App.tsx
@@ -1,118 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <View style={style.container}>
+        <Text style={style.title}>Basic React Native</Text>
+      </View>
+      <Text style={style.subTitle}>Image from URL</Text>
+      <Image
+        style={style.img1}
+        source={{ uri: 'https://i1.sndcdn.com/artworks-lbEUeZGlHxX5wXIQ-b6q4Yw-t500x500.jpg' }}
+      />
+      <Text style={style.subTitle}>Image from Local Directory</Text>
+      <Image
+        style={style.img2}
+        source={require('./assets/image2.jpg')}
+      />
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    borderColor: 'black',
+    borderWidth: 10,
+    margin: 30,
+    borderRadius: 50,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '900',
+    color: 'yellow',
+    textAlign: 'center',
+    margin: 50,
+  },
+  subTitle: {
+    fontSize: 25,
+    fontWeight: '500',
+    textAlign: 'center',
+    marginTop: 30,
+    marginBottom: 10,
+  },
+  img1: {
+    height: 200,
+    width: 200,
+    alignSelf: 'center',
+  },
+  img2: {
+    height: 200,
+    width: 350,
+    alignSelf: 'center',
+  },
+});
