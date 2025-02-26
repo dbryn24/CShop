1df3acddb90c89c4d8e2c69e2d80ff771aebe9f6
dbryn24 <davebryangamer@gmail.com>
2025-02-26 14:13:47 +0800

    flexbox

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 import {name as appName} from './app.json';
-import Exercise3 from './exercise3';
+// import Exercise3 from './exercise3';
+import Flexbox from './Flexbox';
 
-AppRegistry.registerComponent(appName, () => Exercise3);
+AppRegistry.registerComponent(appName, () => Flexbox);
