b6304e7c7c650cb193ff3ce731ca7866fe83ff83
david004-student <s22210250@student.unklab.ac.id>
2025-05-07 01:17:57 +0800

    done bang

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,6 @@ import {StyleSheet, View, Image, Alert} from 'react-native';
 import {Button, Gap} from '../../components/atoms/';
 import {Header, TextInput} from '../../components/molecules/';
 import Logo from '../../assets/pictures/logo.png';
-import TxtButton from '../../components/atoms/TxtButton';
 import {auth, firestore} from '../../config/Firebase';
 import {signInWithEmailAndPassword} from 'firebase/auth';
 import {collection, query, where, getDocs} from 'firebase/firestore';
@@ -33,15 +32,13 @@ const SignIn = ({navigation}) => {
       const userDoc = querySnapshot.docs[0];
       const userData = userDoc.data();
 
-      console.log('User Data:', userData); // Debugging log
-
       // Login menggunakan email dan password
       await signInWithEmailAndPassword(auth, userData.email, password);
 
       Alert.alert('Success', 'You are now signed in!');
       navigation.navigate('Home');
     } catch (error) {
-      console.error('Sign-in error:', error); // Debugging log
+      console.error('Sign-in error:', error);
       Alert.alert('Error', 'Username or Password is incorrect');
     }
   };
@@ -67,11 +64,6 @@ const SignIn = ({navigation}) => {
         secureTextEntry
       />
       <Gap height={28} />
-      <TxtButton
-        label="Forgot password?"
-        onPress={() => navigation.navigate('ForgotPassword')}
-      />
-      <Gap height={32} />
       <Button
         label="Sign In"
         color="#FD7014"
@@ -83,9 +75,7 @@ const SignIn = ({navigation}) => {
         label="Create New Account"
         color="#50577A"
         textColor="#FFFFFF"
-        onPress={() => {navigation.navigate('SignUp');
-          console.log('Navigating to SignUp');
-        }}
+        onPress={() => navigation.navigate('SignUp')}
       />
     </View>
   );
@@ -94,16 +84,16 @@ const SignIn = ({navigation}) => {
 export default SignIn;
 
 const styles = StyleSheet.create({
+  contentContainer: {
+    backgroundColor: '#222831',
+    flex: 1,
+    paddingHorizontal: 24,
+  },
   logoContainer: {
     alignItems: 'center',
   },
   logo: {
     width: 300,
     height: 300,
-  },
-  contentContainer: {
-    backgroundColor: '#222831',
-    flex: 1,
-    paddingHorizontal: 24,
   },
 });
